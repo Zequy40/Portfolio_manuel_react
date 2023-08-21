38f2cfdc882daddfbf40093842851c8a6247acee
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2023-08-21 10:53:18 +0200

    automatisation photo

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -9,38 +9,53 @@ import img3 from './../../public/images/img-02361.png';
 import img4 from './../../public/images/img-06241.png';
 import img5 from './../../public/images/img-06361.png';
 import img6 from './../../public/images/img-07521.png';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 
 export const Photography = () => {
- 
+    const [products, setProducts] = useState([])
+    const folder = '/images/'
+     useEffect(()=>{
+      axios.get('https://manuelcavilla.com/api-image.php').then(response => {
+        setProducts(response.data)
+      })
+      .catch(error => {
+        console.error('Error al descargar los productos:', error);
+      })
+     })
   return (
     <>
     <Header/>
         <div className="max-md:hidden">
            
             <div className={photo.container}>
-                <div className={photo.gallery}>
+            {products.map(product => (
+                <>
+                <div className={photo.gallery} key={product.id}>
                     <div className={photo.normal}>
-                        <img src={img2} className={photo.small}
+                        <img src={folder + product.image} className={photo.small}
                             alt="image" />
 
-                        <img src={img3} className={photo.small}
+                        <img src={folder + product.image} className={photo.small}
                             alt="manuel" />
                     </div>
-                    <img src={img4} className={photo.large}
+                    <img src={folder + product.image} className={photo.large}
                         alt="bl" />
                 </div>
                 <div className={photo.gallery}>
-                    <img src={img6} className={photo.large}
+                    <img src={folder + product.image} className={photo.large}
                         alt="zeki" />
                     <div className={photo.normal}>
-                        <img src={img1} className={photo.small}
+                        <img src={folder + product.image} className={photo.small}
                             alt="image" />
 
-                        <img src={img5} className={photo.small}
+                        <img src={folder + product.image} className={photo.small}
                             alt="image" />
                     </div>
                 </div>
+                </>
+            ))}
             </div>
             
            
