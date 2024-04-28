479513a955deb5f7eb2bd905e40c81f892d7d70f
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2024-04-28 19:20:14 +0200

    improvement wedding page

--- src/components/Wedding.jsx
+++ src/components/Wedding.jsx
@@ -17,9 +17,17 @@ export const Wedding = () => {
             <div className="max-w-[1272px]  m-auto py-5 relative">
             <div className="absolute w-full -top-36 -z-10">
                <img src={photo} alt="" className=''/>
-                <h1>Packages</h1>
+                
                    </div>
-               <div className="relative mt-[35rem]">
+                   <div className='relative mt-[45rem]'>
+                    <h1 className="text-black text-4xl text-center py-10">Packages</h1>
+                    <div className="grid grid-cols-3 gap-x-[200px] max-w-[1272px] mx-auto align-center">
+  <div className='border border-solid border-black text-center px-6'>Contenido de la columna 1</div>
+  <div className='bg-black text-white text-center px-6'>Contenido de la columna 2</div>
+  <div className='border border-solid border-black text-center px-6'>Contenido de la columna 3</div>
+</div>
+                   </div>
+               <div className="relative ">
                 <FormsBlack />
                 <Find />
            </div>
