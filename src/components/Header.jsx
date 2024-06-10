1d62acfac1a798a271ba598e716819dad8959206
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2024-06-10 18:26:44 +0200

    fix the svg header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -105,7 +105,7 @@ useEffect(() => {
         </div>
 
         <div className={`${contact.menu} ${menu ? contact.isActive : ''}`}>
-          <button className={contact.close} onClick={handleClickOff}>
+          <button className={`${contact.close} ${isScrolled ? ' text-black' : ''}`} onClick={handleClickOff}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={contact.svg} viewBox="0 0 16 16">
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
             </svg>
